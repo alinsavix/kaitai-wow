a1002cf498fdcbde4ddc2ab1d94a149faf72e3f0
TDV Alinsa <alinsa-github@lupine.org>
2022-04-14 18:26:14 -0700

    build: add version metadata to module __init__.py

--- wowdump/__init__.py
+++ wowdump/__init__.py
@@ -1,4 +1,10 @@
 # flake8: noqa
 # type: ignore
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = 'unknown'
+
 # FIXME: the listfile stuff here is painful
 from .wowdump import get_default_listfile, main, set_default_listfile
